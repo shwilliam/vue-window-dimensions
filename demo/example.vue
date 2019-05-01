67aab2609c34ac8ea48ef01f1b64b4c5151e2ac1
William L <w-lindvall@outlook.com>
2019-04-30 23:50:53 -0700

    docs: update demo

--- demo/example.vue
+++ demo/example.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="example">
-    <VueWindowDimensions/>
+    <VueWindowDimensions>
+      <template slot-scope="{ dimensions }">
+        <div v-if="dimensions">
+          width: {{ dimensions.width }}
+          height: {{ dimensions.height }}
+        </div>
+      </template>
+    </VueWindowDimensions>
   </div>
 </template>
 
 <script>
-import VueWindowDimensions from "../dist/vue-window-dimensions.esm.js";
+import VueWindowDimensions from '../dist/vue-window-dimensions.esm.js'
 export default {
-  name: "Example",
+  name: 'Example',
   components: {
     VueWindowDimensions
   }
-};
+}
 </script>
 
 <style scoped>
