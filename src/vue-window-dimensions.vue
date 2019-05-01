d878222eb458eada57e785a3f61917481144c191
William L <w-lindvall@outlook.com>
2019-04-30 23:50:41 -0700

    feat: sketch component

--- src/vue-window-dimensions.vue
+++ src/vue-window-dimensions.vue
@@ -3,16 +3,31 @@ export default {
   name: 'VueWindowDimensions',
   data () {
     return {
+      dimensions: null
+    }
+  },
+  mounted () {
+    if (typeof window !== 'undefined') {
+      this.resize()
+      window.addEventListener('resize', this.resize)
+    }
+  },
+  destroyed () {
+    window.removeEventListener('resize', this.resize)
+  },
+  methods: {
+    resize () {
+      this.dimensions = {
+        width: window.innerWidth,
+        height: window.innerHeight
+      }
     }
   }
 }
 </script>
 
 <template>
-  <div>
-    hello
+  <div class="vue-window-dimensions">
+    <slot :dimensions="dimensions" />
   </div>
 </template>
-
-<style scoped>
-</style>
